bd4a4b5109b833629e5f10d2f56db93791743208
sonht <sonht1109@gmail.com>
2021-02-02 00:42:02 +0700

    update progressbar

---
+++ src/customButton/ProgressBar.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react'
+import { Dimensions, StyleSheet, Text, View } from 'react-native'
+import Animated, { Easing } from 'react-native-reanimated'
+
+const { width } = Dimensions.get('window')
+
+export default function ProgressBar() {
+
+    const progress = new Animated.Value(0)
+
+    useEffect(() => {
+        progress.setValue(0)
+        Animated.timing(progress, {
+            toValue: 100,
+            duration: 5000,
+            easing: Easing.linear,
+        }).start()
+    }, [])
+
+    return (
+        <View style={{ justifyContent: 'center' }}>
+            {/* <Text>{value} %</Text> */}
+            <View style={styles.bar}>
+                <Animated.View style={{
+                    height: 14, width: 50,
+                    backgroundColor: 'black',
+                    width: progress.interpolate({
+                        inputRange: [0, 50, 100],
+                        outputRange: [0, (width - 50) / 2, width - 50]
+                    })
+                }} />
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    bar: {
+        borderColor: "black",
+        borderWidth: 1,
+        width: width - 50,
+        height: 16,
+    }
+})
--- src/customButton/index.js
+++ src/customButton/index.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import ScaleButton from './ScaleButton'
 import StatefulButton from './StatefulButton'
+import ProgressBar from './ProgressBar';
 
 export default function CustomButton() {
     return (
         <View style={styles.container}>
             <ScaleButton />
             <StatefulButton />
+            <ProgressBar />
         </View>
     )
 }
